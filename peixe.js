e294cadf58039c14981ba0e34d250a042e9efeef
Sofia <sofia.arone@sou.inteli.edu.br>
2025-02-06 14:11:49 -0300

    Update peixe.js

--- peixe.js
+++ peixe.js
@@ -14,13 +14,17 @@ var config= {
 var game= new Phaser.Game(config);
 var peixinho;
 function preload(){ 
-    this.load.image('mar', 'bg_azul-escuro.png');
-    this.load.image('logo', 'logo-inteli_branco.png');
-    this.load.image('peixe', 'peixes/tubarao.png');
+    this.load.image('mar', 'assets/bg_azul-escuro.png');
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
+    this.load.image('peixe', 'assets/peixes/tubarao.png');
+    this.load.image('concha', 'assets/concha.png');
+
 }
 function create(){
     this.add.image(400, 300, 'mar');
     this.add.image(400, 525, 'logo').setScale(0.5);
+    this.add.image(200, 250, 'concha');
+
    peixinho= this.add.image(400, 300, 'peixe').setOrigin(0.5, 0.5).setFlip( true, false);
    peixinho.setFlip(true, false);
 }
