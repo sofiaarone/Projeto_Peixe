9ffd917d5a76b5d3cc40e0e152b56d122404e4f6
Sofia <sofia.arone@sou.inteli.edu.br>
2025-02-05 21:49:10 -0300

    Add files via upload

---
+++ peixe.js
@@ -0,0 +1,31 @@
+var peixinho;
+
+var config= {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update 
+    }
+};
+
+var game= new Phaser.Game(config);
+var peixinho;
+function preload(){ 
+    this.load.image('mar', 'bg_azul-escuro.png');
+    this.load.image('logo', 'logo-inteli_branco.png');
+    this.load.image('peixe', 'peixes/tubarao.png');
+}
+function create(){
+    this.add.image(400, 300, 'mar');
+    this.add.image(400, 525, 'logo').setScale(0.5);
+   peixinho= this.add.image(400, 300, 'peixe').setOrigin(0.5, 0.5).setFlip( true, false);
+   peixinho.setFlip(true, false);
+}
+
+function update (){ 
+    peixinho.x = this.input.x;
+    peixinho.y = this.input.y;
+}
